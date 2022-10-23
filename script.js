a314212d706f118ca8773018d462c21a22a4a68c
Phạm Văn Tới <64025019+ToiPhamOfficial@users.noreply.github.com>
2022-10-23 12:59:47 +0700

    script.js

--- script.js
+++ script.js
@@ -91,35 +91,6 @@ const app = {
       path: "https://github.com/ToiPhamOfficial/profile/blob/main/Upload%20Files/Safari%20-%20Serena.mp3?raw=true",
       image: "https://github.com/ToiPhamOfficial/profile/raw/main/Upload%20Files/Images/IMG_1917.JPG"
     },
-    {
-      name: "Mantoiyat",
-      singer: "Raftaar x Nawazuddin Siddiqui",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
-      image:
-        "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
-    },
-    {
-      name: "Aage Chal",
-      singer: "Raftaar",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
-      image:
-        "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
-    },
-    {
-      name: "Damn",
-      singer: "Raftaar x kr$na",
-      path:
-        "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
-      image:
-        "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg"
-    },
-    {
-      name: "Feeling You",
-      singer: "Raftaar x Harjas",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
-      image:
-        "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
-    }
   ],
   setConfig: function (key, value) {
     this.config[key] = value;
